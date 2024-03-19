c0cb17360b8438d1d9b16e0f6428c55dae9c9c29
Agafina <98034773+Agafina@users.noreply.github.com>
2024-03-19 14:29:34 +0100

    new button

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import BookList from './components/BookList';
 import Navbar from './components/Navbar';
-import ThemeToggle from './components/ThemeToggle';
+import ThemeToggle from './components/ThemeToggle.js';
 import ThemeContextProvider from './context/ThemeContext';
 
 
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,29 +1,19 @@
-import React, { Component } from 'react';
+import React, {  useContext } from 'react';
 import { ThemeContext } from '../context/ThemeContext';
 
 
-class BookList extends Component {
-
-    render() { 
-       return (
-        <ThemeContext.Consumer>{(ThemeContext) => {
-            const {isLightTheme, light, dark} = ThemeContext
-            const theme = isLightTheme ? light : dark
-            return (
-                <div className='booklist' style={{color:theme.syntax, background:theme.bg}}>
+const BookList = () => {
+        const {isLightTheme, light, dark} = useContext(ThemeContext)
+        const theme = isLightTheme ? light :dark;
+    return ( 
+        <div className='booklist' style={{color:theme.syntax, background:theme.bg}}>
                     <ul>
                         <li style={{background:theme.ui}}>The way of kings</li>
                         <li style={{background:theme.ui}}>Taken away by lightening</li>
                         <li style={{background:theme.ui}}>Trying to make things better</li>
                     </ul>
                 </div>
-            );
-           } }
-    
-           </ThemeContext.Consumer>
-        
-       );
-    }
+     );
 }
  
 export default BookList;
