6bee71e5d37ade95394c50a2b1b90d3abf8aec48
Agafina <98034773+Agafina@users.noreply.github.com>
2024-03-19 12:49:33 +0100

    added a button to toggle between themes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@
 import './App.css';
 import BookList from './components/BookList';
 import Navbar from './components/Navbar';
+import ThemeToggle from './components/ThemeToggle';
 import ThemeContextProvider from './context/ThemeContext';
+
 
 function App() {
   return (
     <div className="App">
-     <ThemeContextProvider>
+   
+    <ThemeContextProvider>
         <Navbar />
         <BookList />
+        <ThemeToggle />
      </ThemeContextProvider>
+   
     </div>
   );
 }
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -8,9 +8,12 @@ class ThemeContextProvider extends Component {
         light: {syntax:'#555', ui:'#ddd', bg:"#eee"},
         dark:{syntax:"#ddd", ui:'#333', bg:'#555'}
     } 
+    ThemeToggle = () =>{
+        this.setState({isLightTheme : !this.state.isLightTheme})
+    }
     render() { 
         return (
-            <ThemeContext.Provider value={{...this.state}}>
+            <ThemeContext.Provider value={{...this.state, ThemeToggle:this.ThemeToggle}}>
                 {this.props.children}
             </ThemeContext.Provider>
         );
